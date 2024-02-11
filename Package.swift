98830218cd9c3479d92616c6e6b0eadb85b4bd54
Byung jin <bjcho1503@naver.com>
2024-02-11 22:51:59 +0900

    💉 :: 의존성 추가

--- Package.swift
+++ Package.swift
@@ -1,15 +1,24 @@
+// swift-tools-version:5.9
 import PackageDescription
 
 let package = Package(
     name: "Toasty",
     products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "Toasty",
             targets: ["Toasty"]),
     ],
+    dependencies: [
+        .package(url: "https://github.com/SnapKit/SnapKit.git", .upToNextMajor(from: "5.7.0"))
+    ],
     targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "Toasty"),
+            name: "Toasty",
+            dependencies: ["SnapKit"]
+        ),
         .testTarget(
             name: "ToastyTests",
             dependencies: ["Toasty"]),
